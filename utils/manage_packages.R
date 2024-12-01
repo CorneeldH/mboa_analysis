db19e83897e523c466c6ecde139ca16f29528c39
corneeldh <corneel@cefero.nl>
2024-12-01 09:29:27 +0100

    add: utils, first version with too much packages

---
+++ utils/manage_packages.R
@@ -0,0 +1,105 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## R code for MBOA Analysis - Cefero
+## This source code is licensed under the MIT license found in the
+## LICENSE file in the root directory of this repository.
+## Copyright 2024 Cefero
+## Web Page: www.cefero.nl
+## Contact: corneel@cefero.nl
+##
+##' *INFO*:
+## 1) ___
+##
+## ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+## ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+## 1. SET-UP ####
+## ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+# In order for load
+packages_base <- c(
+    "base",
+    "methods",
+    "utils",
+    "stats",
+    "graphics",
+    "grDevices",
+    "datasets")
+
+
+# TODO When updating this list, also run outcommented code
+packages_cran <- c(
+
+    # develop package
+    "devtools",
+    "usethis",
+    "roxygen2",
+
+
+    "LaF",            # Read data files without encoding (like ASCII)
+    "dataReporter",   # Create a data audit report
+    "rlang",          # Enable complex operations
+    "config",         # Set up configuration files and functions
+    "janitor",        # Clean up names from special characters
+    "lubridate",      # Work with dates and times
+    "purrr",          # Work with functions and vectors
+    #"readxl",         # Read xlsx
+    "readr",          # Read data (csv, tsv, and fwf)
+    #"rvest",          # Read html
+    "slackr",         # Send messages in Slack
+    "stringi",        # Work with other strings
+    "stringr",        # Work with strings
+    "tibble",         # Edit and create tibbles
+    "tidyr",          # Tidy data in the tidyverse environment
+    "utils",          # Provide utility functions
+    #"fst",            # Perform operations with large data files
+    "dplyr",          # Utilise the dplyr environment
+    "vvmover",
+    "vvconverter"
+)
+
+# Include both the package name (for loading) and the account name (for renv snapshot)
+packages_github <- c(
+    "vusa"            # Utilise packages from the VU team
+)
+
+packages_github_with_account <- c(
+    "vusaverse/vusa"  # Utilise packages from the VU team
+)
+
+
+# Combine packages, config should not be loaded
+packages <- c(packages_base, packages_cran, packages_github)
+packages <- packages[packages != "config"]
+packages_renv <- c(packages_cran, packages_github)
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## 2. EXECUTE ####
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# TODO Github packages installation is a bit weird
+# renv:install(packages_github_with_account)
+
+options(renv.snapshot.filter = function(project) {
+    return(packages_renv)
+})
+
+# TODO Un-edit when adding packages above to include them in snapshot
+# renv::snapshot(type = "custom")
+
+renv::restore()
+
+
+# Load packages
+# TODO Set to TRUE when adding packages to check if there are problematic conflicts
+warn_conflicts <- FALSE
+suppressMessages(purrr::walk(packages, ~library(.x,
+                                                character.only = TRUE,
+                                                warn.conflicts = warn_conflicts)))
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## WRITE-AND-CLEAR ####
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+clear_script_objects()
