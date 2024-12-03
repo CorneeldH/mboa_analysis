08a65cf91ce55c7a3f874f779ce021fbf3928d8c
corneeldh <corneel@cefero.nl>
2024-12-03 20:56:36 +0100

    add: helper for checking package

---
+++ utils/update_package.R
@@ -0,0 +1,17 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## R code for MBOA Analysis - Cefero
+## This source code is licensed under the MIT license found in the
+## LICENSE file in the root directory of this repository.
+## Copyright 2024 Cefero
+## Web Page: www.cefero.nl
+## Contact: corneel@cefero.nl
+##
+##' *INFO*:
+## 1) ___
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+devtools::load_all()
+devtools::document()
+check_result <- devtools::check()
+print(check_result)
